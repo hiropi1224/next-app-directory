dd77ee7d507bce433b22b4c21ea32bc464805d51
Shirahamah <hiropi1224s@gmail.com>
2023-04-14 22:20:36 +0900

    💄 ルートレイアウト追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from './components/nav-bar'
 import './globals.css'
 
 export const metadata = {
@@ -11,8 +12,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ja">
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
