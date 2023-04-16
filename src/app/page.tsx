0b8b5257134f5189ff24084085dd5e715cfdbbf6
Shirahamah <hiropi1224s@gmail.com>
2023-04-16 14:23:09 +0900

    client componentを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from 'react'
 import NotesList from './components/notes-list'
+import TimerCounter from './components/timer-counter'
+import Spinner from './components/spinner'
 
 export default function Page() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Next.js app directory</p>
-        {/* @ts-expect-error Async Server Component */}
-        <NotesList />
+        <Suspense fallback={<Spinner />}>
+          {/* @ts-expect-error Async Server Component */}
+          <NotesList />
+        </Suspense>
+        <TimerCounter />
       </div>
     </main>
   )
